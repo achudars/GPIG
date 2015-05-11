e126a44e2114d2b6f985e91d35dbee812a1865ac
Henri Normak <henri.normak@gmail.com>
2015-05-11 17:24:33 +0100

    Added a basic script for CSV > SHP conversion
    Also included a database dump (which, I know, as a big file does not belong in a repo, so don't sue). Also added some basic installation instructions to README.

---
+++ Database/csv2shp.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+import fnmatch
+import sys
+import time
+import shapefile as shp
+import csv
+
+# Readable directory function
+def readable_dir(prospective_dir):
+  if not os.path.isdir(prospective_dir):
+    raise Exception("{0} is not a valid path".format(prospective_dir))
+  if os.access(prospective_dir, os.R_OK):
+    return prospective_dir
+  else:
+    raise Exception("{0} is not a readable dir".format(prospective_dir))
+
+# Progress reporting
+def progress(count, total, suffix=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
+    sys.stdout.flush()
+
+# Arguments for the script
+parser = argparse.ArgumentParser(description='Parse CSV files to Shapefile')
+parser.add_argument('-d', '--directory', help='Input directory to check')
+parser.add_argument('-o', '--output', help='Output location', default='converted')
+parser.add_argument('-r', '--recursive', action='store_true', help="Set to look for CSV files recursively")
+
+args = parser.parse_args()
+
+# Validate input directory
+args.directory = readable_dir(os.path.abspath(args.directory) if args.directory is not None else os.getcwd())
+
+# Collect all CSV files in question
+csv_files = []
+
+if args.recursive:
+    for root, directories, filenames in os.walk(args.directory):
+        for filename in fnmatch.filter(filenames, '*.csv'):
+            csv_files.append(os.path.join(root, filename))
+else:
+    for item in fnmatch.filter(os.listdir(args.directory), '*.csv'):
+        if os.path.isfile(os.path.join(args.directory, item)):
+            csv_files.append(os.path.join(args.directory, item))
+
+count = len(csv_files)
+
+if count == 0:
+    print "Found no CSV files, exiting"
+    sys.exit(0)
+
+print "Found " + str(count) + " CSV files"
+print "Starting parsing into " + args.output
+
+# Crime conversion table
+crimes = {'Other crime': 'other-crime', 'Violent crime': 'violent-crime', 'Vehicle crime': 'vehicel-crime',
+'Shoplifting': 'shoplifting', 'Robbery': 'robbery', 'Public disorder and weapons': 'public-disorder-weapons',
+'Other theft': 'other-theft', 'Drugs': 'drugs', 'Criminal damage and arson': 'criminal-damage-arson',
+'Burglary': 'burglary', 'Anti-social behaviour': 'anti-social-behaviour', 'All crime': 'all-crime'}
+
+# Shape writer
+w = shp.Writer(shp.POINT)
+w.autobalance = 1
+
+# Field names, from CSV structure of data.police.uk data
+# Crime ID,Month,Reported by,Falls within,Longitude,Latitude,Location,LSOA code,LSOA name,Crime type,Last outcome category,Context
+w.field('crime', 'C', 26)
+w.field('jurisdiction', 'C', 35)
+w.field('date', 'D')
+
+for i, file in enumerate(csv_files):
+    with open(file, 'rb') as csvfile:
+        r = csv.reader(csvfile, delimiter=',')
+
+        # Skip the first row (header)
+        r.next()
+
+        for _, row in enumerate(r):
+            # Ignore rows with no location data
+            if row[4] == '' or row[5] == '':
+                continue
+
+            w.point(float(row[4]), float(row[5]))
+
+            # Attributes are done in a single swoop
+            crime = (crimes[row[9]] if row[9] in crimes else 'all-crime')
+            jurisdiction = row[3]
+            date = ''.join(row[1].split('-')) + '01'
+            w.record(crime, jurisdiction, date)
+
+    progress(i + 1, count)
+
+print "\nParsing done, saving files"
+
+# Save the shapefile
+w.save(args.output)
+
+print "Conversion finished"
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # GPIG
+
+## Database installation/restoration
+
+Database should be running on PostgreSQL, which on OS X is quite easy to set up (pending Windows guide), simply download [Postgres.app](https://github.com/PostgresApp/PostgresApp/releases) and run it. This will come with all the tools you need for the database server (including command-line tools).
+
+Assuming you have PostgreSQL server running, and PostGIS installed, you can restore a database of roughly 6.5 million incidents from 2014-01 to 2015-03 (not verified) as follows. Run these commands from the `Database` directory
+
+```bash
+createdb crimedata
+gunzip -c data.gz | psql crimedata
+```
+
+Assuming the database restores without problems, you should be able to setup the PostGIS resource under GeoServer referencing [this guide](http://docs.geoserver.org/stable/en/user/data/database/postgis.html) (not verified).
