ce40c6f3af7226a5d059af2d6bff79d91c36473a
FiifiB <fidjasi@gmail.com>
2015-05-27 00:17:41 +0100

    Some more javascript for inactive file atm.

--- Applications/presentation/src/app/PoliceDistribution.js
+++ Applications/presentation/src/app/PoliceDistribution.js
@@ -3,7 +3,7 @@ if (!window.app) {
 }
 var app = window.app;
 
-app.PoliceDistribution = $(function() {
+$(function() {
     var action;
     $(".number-spinner button").mousedown(function () {
         btn = $(this);
@@ -33,3 +33,58 @@ app.PoliceDistribution = $(function() {
         clearInterval(action);
     });
 });
+
+var noOfPolice = 10;
+var neighbourhoodCrimecounts = [];
+var distroPercentage = [];
+var policeDistribution = [];
+
+function calculateDistribution () {
+    //alert(document.getElementById("policeNo").value);
+    //var policeDistro = new ol.
+    //var policeCluster = new 
+    //map.addLayer();
+    for (evt in selectedNeighbourhoods){
+        neighbourhoodCrimecounts.push(parseInt(neighbourhoodsStatsSource.getFeaturesAtCoordinate(selectedNeighbourhoods[evt])[0].get("crimecount")));
+    }
+    var sum = 0;
+    
+    for(x in neighbourhoodCrimecounts){
+        sum = sum + parseInt(neighbourhoodCrimecounts[x]);
+    }
+    console.log(neighbourhoodCrimecounts);
+    for(x in neighbourhoodCrimecounts){
+        distroPercentage.push(neighbourhoodCrimecounts[x] / sum);
+    }
+    
+    noOfPolice = document.getElementById("policeNo").value;
+    
+    for(x in distroPercentage){
+        policeDistribution.push(Math.round(distroPercentage[x] * noOfPolice));
+    }
+    console.log(policeDistribution);
+    
+    // create a vector layer to contain the police distribution centroids 
+    var policeDistroLayer = new ol.layer.Vector({
+        title: "Police Distribution",
+        source: new ol.source.Vector(),
+        style:function(feature, resolution) {
+                return app.sharedStyle.generateNeighbourhoodStyle(feature, resolution);
+            }        
+    });
+    
+    for(x in policeDistribution){
+        feature = new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.transform(selectedNeighbourhoods[x], 'EPSG:4326', 'EPSG:3857')),
+            name: selectedNeighbourhoodsNames[x],
+            noofpolice: policeDistribution[x]
+            });
+        policeDistroLayer.getSource().addFeature(feature);
+    }
+    policeDistroLayer.setVisible(true);
+    console.log(policeDistroLayer.getSource());
+    
+    //map.addLayer(policeDistroLayer);
+    //console.log(neighbourhoodsStatsSource);
+    $("#policeModal").modal('hide');
+}
